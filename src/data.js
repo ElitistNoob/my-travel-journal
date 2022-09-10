838a44f707f051c742d84aa73e2ab0e3cbd50f2f
Kevin Hernot <heavykevin2000@hotmail.com>
2022-09-10 19:14:07 +0700

    Added data to dada

---
+++ src/data.js
@@ -0,0 +1,100 @@
+const data = [
+  {
+    id: "1",
+    title: "France",
+    location: "Europe",
+    googleMapsUrl: "https://goo.gl/maps/VfcGLSxSjABRF16eA",
+    startDate: "jun, 2004",
+    endDate: "jul, 2004",
+    description:
+      "France, in Western Europe, encompasses medieval cities, alpine villages and Mediterranean beaches.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNcuKqC3iWIpVrQB_NgJW9GRHqYjtTx2MgFLLAV=w408-h615-k-no",
+  },
+  {
+    id: "2",
+    title: "Bolivia",
+    location: "South America",
+    googleMapsUrl: "https://goo.gl/maps/ecd24iMiot8Nd27S6",
+    startDate: "April, 2008",
+    endDate: "May, 2008",
+    description:
+      "Bolivia is a country in central South America, with a varied terrain spanning Andes Mountains, the Atacama Desert and Amazon Basin rainforest.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNhUD0MhFTB3VDQdRkA1_LKT8L_r1mv9rQHxF59=w408-h302-k-no",
+  },
+  {
+    id: "3",
+    title: "Australia",
+    location: "Oceania",
+    googleMapsUrl: "https://goo.gl/maps/vn9ntP57gCmTpZXy5",
+    startDate: "Jun, 2010",
+    endDate: "Nov, 2010",
+    description:
+      "Australia, officially the Commonwealth of Australia, is a sovereign country comprising the mainland of the Australian continent, the island of Tasmania, and numerous smaller islands.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipMTvE7PlGoKnGJfX9M5sX-6J2xTs30zGSkkZ38=w408-h272-k-no",
+  },
+  {
+    id: "4",
+    title: "Indonesia",
+    location: "Southeast Asia",
+    googleMapsUrl: "https://goo.gl/maps/YoxXJnFmNbtBdh627",
+    startDate: "Nov, 2010",
+    endDate: "Dec, 2010",
+    description:
+      "Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNwNLrZ0uLq2-Qtm2iShBmoUvZenl-1VSO2Dknm=w408-h510-k-no",
+  },
+  {
+    id: "5",
+    title: "Thailand",
+    location: "Southeast Asia",
+    googleMapsUrl: "https://goo.gl/maps/5M4DVpEVYaKRfDpAA",
+    startDate: "Oct, 2012",
+    endDate: "Nov, 2012",
+    description:
+      "Thailand is a Southeast Asian country. It's known for tropical beaches, opulent royal palaces, ancient ruins and ornate temples displaying figures of Buddha.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipO4KXWU5JrlskPIuCuYsnr4JvWXByfKfsWwEEZN=w408-h272-k-no",
+  },
+  {
+    id: "6",
+    title: "Laos",
+    location: "Southeast Asia",
+    googleMapsUrl: "https://goo.gl/maps/AstVHqwnHVuJCizeA",
+    startDate: "Nov, 2012",
+    endDate: "Dec, 2012",
+    description:
+      "Laos is a Southeast Asian country traversed by the Mekong River and known for mountainous terrain, French colonial architecture, hill tribe settlements and Buddhist monasteries.",
+    imageUrl:
+      "https://lh4.googleusercontent.com/proxy/9LF0U29sB8AQuCi-jLAmuDOgQyHc1kzc_D7GEKOSXrwbwmLboZuYrwcG8Gxz_TeHn3BnCXG9NrNmSM9WwCDpIyZ39xJmJb32ZVArPDUBYxchNQ25UaIMHkTDOYe0ANODNwTo3T47j1IxB4jxMHRnXL_ySwC9ZPc=w472-h240-k-no",
+  },
+  {
+    id: "7",
+    title: "Vietnam",
+    location: "Southeast Asia",
+    googleMapsUrl: "https://goo.gl/maps/zisvupt1h7k8QsdB8",
+    startDate: "Dec, 2012",
+    endDate: "Jan, 2012",
+    description:
+      "Vietnam is a Southeast Asian country known for its beaches, rivers, Buddhist pagodas and bustling cities.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNn685swdKmyZI6glPm0Nko37KyslYvBk_hY-GE=w533-h240-k-no",
+  },
+  {
+    id: "8",
+    title: "Cambodia",
+    location: "Southeast Asia",
+    googleMapsUrl: "https://goo.gl/maps/pjEypTeER5h1pzJ67",
+    startDate: "Jan, 2012",
+    endDate: "Present",
+    description:
+      "Cambodia is a Southeast Asian nation whose landscape spans low-lying plains, the Mekong Delta, mountains and Gulf of Thailand coastline.",
+    imageUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipO5-3uI7hn6XI4r6OLqbAnm_vlaP8VgzX2fAH2B=w426-h240-k-no",
+  },
+];
+
+export default data;
